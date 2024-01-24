f0a02cd67b70a8fe475205da81dbce82dbbfb73c
Muhammad Saqlain <154231070+saqlainshabbir@users.noreply.github.com>
2024-01-23 23:10:49 -0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+import requests
+import argparse
+
+def send_slack_message(message: str):
+    '''
+    Send a message to our slack channel.
+    '''
+    payload = '{"text": "%s"}' % message
+
+    response = requests.post(
+        'https://hooks.slack.com/services/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',
+        data=payload
+    )
+    print(response.text)
+
+def chat_with_user():
+    '''
+    Function to chat with the user and get a message.
+    '''
+    user_message = input('Enter your message: ')
+    return user_message
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Send and receive messages with ChatGPT')
+    args = parser.parse_args()
+
+    user_input = chat_with_user()
+
+    if len(user_input) > 0:
+        send_slack_message(message=user_input)  # Fix the argument name here
+    else:
+        print('Please enter a message!')
